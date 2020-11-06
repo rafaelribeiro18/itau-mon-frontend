50cd11b70c88ca16103d67534c2bc4f2c2929809
Marcelo Martins <marcelonam@gmail.com>
2020-11-06 11:44:45 -0300

    .

--- js/eventos.js
+++ js/eventos.js
@@ -37,7 +37,17 @@ function gerarRelatorioEventos(){
 }
 
 function preencheEventos(dados) {
-    var tabela = '<table class="table table-sm text-light"> <tr><th>Data</th> <th>Alarme</th> <th>Equipamento</th></tr>';
+    console.log(dados);
+    if (dados.length == 0) {
+        var tabela = '<table class="table table-sm text-light"> <td><h5>Nenhum registro encontrado no periodo selecionado</h5></td>';
+
+    tabela = tabela + "</table>"
+    document.getElementById("tabela").innerHTML = tabela;
+    document.getElementById("btn_export").innerHTML = null;
+
+    } else {    
+    console.log(dados);
+        var tabela = '<table class="table table-sm text-light"> <tr><th>Data</th> <th>Alarme</th> <th>Equipamento</th></tr>';
     for (i = 0; i < dados.length; i++) {
         tabela = tabela + ` <tr> 
                                 <td>${new Date(dados[i].dataevt).toLocaleString("pt-BR")}</td>
@@ -45,7 +55,66 @@ function preencheEventos(dados) {
                                 <td>${dados[i].equipamento.hostname} </td> 
                             </tr>`        
     }
-
     tabela = tabela + "</table>"
     document.getElementById("tabela").innerHTML = tabela;
+    
+    var botao = `<button class="btn btn-primary" id="btn1" onclick="exportTableToCSV('eventos_periodo.csv')">Exportar (CSV)</button>`;
+    var botao1 = `<button class="btn btn-primary" id="btn1" onclick="CriaPDF();">Imprimir</button>`;
+
+    document.getElementById("btn_export").innerHTML = botao + botao1;
 }
+
+}
+
+function exportTableToCSV(filename) {
+    var csv = [];
+    var rows = document.querySelectorAll("table tr");
+    for (var i = 0; i < rows.length; i++) {
+        var row = [], cols = rows[i].querySelectorAll("td, th");
+        for (var j = 0; j < cols.length; j++) 
+            row.push(cols[j].innerText);
+        csv.push(row.join(","));        
+    }
+    // Download CSV file
+    downloadCSV(csv.join("\n"), filename);
+}
+
+function downloadCSV(csv, filename) {
+    var csvFile;
+    var downloadLink;
+    // CSV file
+    csvFile = new Blob([csv], {type: "text/csv"});
+    // Download link
+    downloadLink = document.createElement("a");
+    // File name
+    downloadLink.download = filename;
+    // Create a link to the file
+    downloadLink.href = window.URL.createObjectURL(csvFile);
+    // Hide download link
+    downloadLink.style.display = "none";
+    // Add the link to DOM
+    document.body.appendChild(downloadLink);
+    // Click download link
+    downloadLink.click();
+}
+
+function CriaPDF() {
+    var minhaTabela = document.getElementById("tabela").innerHTML;
+    var style = "<style>";
+    style = style + "table {width: 100%;font: 20px Calibri;}";
+    style = style + "table, th, td {border: solid 1px #DDD; border-collapse: collapse;";
+    style = style + "padding: 2px 3px;text-align: center;}";
+    style = style + "</style>";
+    // CRIA UM OBJETO WINDOW
+    var win = window.open('', '', 'height=700,width=700');
+    win.document.write('<html><head>');
+    win.document.write('<title>Empregados</title>');   // <title> CABEÇALHO DO PDF.
+    win.document.write(style);                                     // INCLUI UM ESTILO NA TAB HEAD
+    win.document.write('</head>');
+    win.document.write('<body>');
+    win.document.write(minhaTabela);                          // O CONTEUDO DA TABELA DENTRO DA TAG BODY
+    win.document.write('</body></html>');
+    win.document.close();                                            // FECHA A JANELA
+    win.print();                                                            // IMPRIME O CONTEUDO
+}
+
